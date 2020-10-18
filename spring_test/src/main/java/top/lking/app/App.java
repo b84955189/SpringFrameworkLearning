ab78304862635ccbb4cda84b000f96818d9ae52d
Jason Liu <b84955189@vip.qq.com>
2020-10-19 07:52:35 +0800

    CGLIB动态代理实现方式 实践(验证同一个代理对象同一时刻代理多个目标类)

--- spring_test/src/main/java/top/lking/app/App.java
+++ spring_test/src/main/java/top/lking/app/App.java
@@ -1,6 +1,7 @@
 package top.lking.app;
 
 import top.lking.dao.UserDao;
+import top.lking.dao.impl.NormalStudentDaoImpl;
 import top.lking.dao.impl.NormalUserDaoImpl;
 import top.lking.dao.impl.SimpleStudentDaoImpl;
 import top.lking.dao.impl.SimpleUserDaoImpl;
@@ -23,7 +24,11 @@ public class App {
         //----------CGLIB动态代理--------------
         StudentDaoProxy studentDaoProxy = new StudentDaoProxy();
         SimpleStudentDaoImpl simpleStudentDao = new SimpleStudentDaoImpl();
+        NormalStudentDaoImpl normalStudentDao = new NormalStudentDaoImpl();
         SimpleStudentDaoImpl studentProxy = (SimpleStudentDaoImpl)studentDaoProxy.createProxy(simpleStudentDao);
+        NormalStudentDaoImpl normalStudentProxy = (NormalStudentDaoImpl) studentDaoProxy.createProxy(normalStudentDao);
+
         studentProxy.addStudent();
+        normalStudentProxy.addStudent();
     }
 }
